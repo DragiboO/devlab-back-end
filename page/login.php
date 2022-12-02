b4a397f8c407d8fd2017181131136bf47f9885e6
EthraDev <thea.blachon73@gmail.com>
2022-12-02 14:45:53 +0100

    :memo:login.php

--- page/login.php
+++ page/login.php
@@ -14,7 +14,7 @@
     <div class="w-[500px] h-auto text-lg text-center mt-[15vh]">
 
         <form method="POST" class="flex flex-col items-center bg-orange-500 rounded-3xl p-10 gap-y-10 mb-4">
-                <input type="email" name="email" placeholder="email" class="rounded-lg p-2 text-black">
+                <input type="pseudo" name="pseudo" placeholder="pseudo" class="rounded-lg p-2 text-black">
                 <input type="password" name="password" placeholder="password" class="rounded-lg p-2 text-black">
                 <input type="submit" value="Login" name="login">
             </form>
