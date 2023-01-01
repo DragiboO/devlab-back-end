9a6ebbd5fd18d210cc3f46b29b8f2b77accc3566
Thea Blachon <thea.blachon73@gmail.com>
2023-01-01 20:29:31 +0100

    :memo:deconnection fonctionnelle

--- page/myprofile.php
+++ page/myprofile.php
@@ -6,5 +6,25 @@
     <title>Mon Profile</title>
 </head>
 
+<main>
+    <form method="POST">
+        <button name="disconnect" type="submit">Se déconnecter</button>
+    </form>
+</main>
+
+<?php
+
+require_once 'class/user.php';
+require_once 'class/connection.php';
+$connection = new Connection();
+
+if(isset($_POST['disconnect'])){
+    unset($_SESSION['user_id']);
+    header('Location: ../index.php');
+}
+
+?>
+
+<?php require "footer.php"?>
 </body>
 </html>
