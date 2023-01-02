1121a644ece74ade230d7cce23d86f3c3abd6045
Thea Blachon <thea.blachon73@gmail.com>
2023-01-02 18:02:40 +0100

    :art:changement taille des polices sur l'index et position absolute sur la one page des films

--- assets/js/onepage.js
+++ assets/js/onepage.js
@@ -9,11 +9,11 @@ fetch(url_onepage).then((response) => response.json()).then(function(data) {
                 <div>
                     <img src="${"https://image.tmdb.org/t/p/original" + data.backdrop_path}" alt="affiche_du_film" class="h-[70vh] w-full object-cover blur-sm relative">
                     <div class="background-onepage-moovie absolute h-[71vh] w-full top-[90px] left-0"></div>
-                    <img src="${"https://image.tmdb.org/t/p/original" + data.poster_path}" alt="affiche_du_film" class="h-[60vh] absolute bottom-[110px] left-[300px]">
-                    <h2 class="absolute top-[720px] left-[700px] text-3xl">${data.title}</h2>
+                    <img src="${"https://image.tmdb.org/t/p/original" + data.poster_path}" alt="affiche_du_film" class="h-[60vh] absolute bottom-[10%] left-[13%]">
+                    <h2 class="absolute top-[74%] left-[39%] text-3xl">${data.title}</h2>
                 </div>
                 <div class="mt-10 grid grid-cols-3 pl-[10em]">
-                    <div class="mt-[4.5rem] ml-36 text-xl">
+                    <div class="mt-[4.5rem] ml-24 text-xl">
                         <h2>Ajouter à une liste</h2>
                     </div>
                     <div class="flex gap-y-4 flex-col">
