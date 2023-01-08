0c4e949728fd5ad6490fb3dabdc61d14e4897cfb
Thea Blachon <thea.blachon73@gmail.com>
2023-01-08 14:12:21 +0100

    :art: ajout style de la page album et ajout demande de partage + responsive de ceux-ci

---
+++ page/album.php
@@ -0,0 +1,82 @@
+<?php
+require "header.php";
+?>
+
+<h2 class="xl:text-xl text-center py-4">Nom de l'album</h2>
+
+<div class="flex flex-row text-sm px-4 gap-x-14 pb-2 sm:text-lg sm:gap-x-24 sm:px-10 lg:px-16 lg:text-xl lg:gap-x-28 xl:text-xl xl:px-24 xl:gap-x-32 xl:pb-2 border-b-[1px] border-gray-700">
+    <h2>Affiche</h2>
+    <h2>Titre</h2>
+    <h2>durée</h2>
+</div>
+
+<div class="flex flex-col mb-4">
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 border-b-[1px] border-gray-700 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+    <div class="flex flex-row items-center gap-x-6 px-4 pb-2 mt-2 sm:px-10 lg:px-16 xl:gap-x-10 xl:px-24 xl:pb-6 xl:mt-6">
+        <img src="../assets/image/avatar.jpg" alt="img_film" class="w-14">
+        <div class="flex flex-row gap-x-4 sm:gap-x-6 text-xs sm:text-sm lg:text-lg xl:gap-x-12 xl:text-lg">
+            <h2>Avatar 2 : La voie de l'eau</h2>
+            <h2>190min</h2>
+            <p>Retirer de l'album</p>
+        </div>
+    </div>
+</div>
+
+<?php
+require "footer.php";
+?>
--- page/myprofile.php
+++ page/myprofile.php
@@ -7,6 +7,13 @@
 </head>
 
 <main>
+
+    <div class="flex flex-row text-xs gap-x-2 mx-2 mt-2 sm:text-sm sm:w-[50%] sm:mx-10 sm:gap-x-6 sm:mt-6 lg:text-lg lg:mx-14 lg:gap-x-10 lg:px-8 xl:gap-x-10 bg-black rounded-lg xl:mx-40 xl:w-[35%] p-4 xl:px-10">
+        <h2>User1234 veut vous partager son album.</h2>
+        <img src="../assets/image/accepter.png" alt="accepter" class="w-4">
+        <img src="../assets/image/refuser.png" alt="refuser" class="w-4">
+    </div>
+
     <div class="flex justify-between sm:px-14 xl:px-40 xl:py-8 px-10 py-6">
         <div>
 
@@ -21,7 +28,7 @@
     <h2 class="px-10 py-2 text-xs sm:text-base sm:px-14 xl:px-40 xl:py-8 xl:text-xl">Mes albums</h2>
     <div class="grid grid-cols-1 gap-y-6 px-10 mb-6 sm:grid-cols-2 sm:px-14 sm:gap-y-8 sm:gap-x-8 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-10 xl:px-40 xl:mb-8 xl:gap-y-10">
         <div class="relative">
-            <img src="../assets/image/test.webp" alt="test">
+            <a href="album.php"><img src="../assets/image/test.webp" alt="test"></a>
             <h2 class="p-2 text-xs sm:text-base xl:text-xl absolute left-0 bottom-0 bg-orange-500 w-full">Watchlist</h2>
         </div>
         <div class="relative">
